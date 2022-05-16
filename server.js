85d320919d53dfa0e5fd683c968bb6763f68f0f5
Wilhemina <bordeauxwilhemina@gmail.com>
2022-05-15 18:09:24 -0700

    Reversi: Web Sockets Basic.

--- server.js
+++ server.js
@@ -29,3 +29,41 @@ let app = http.createServer(
 ).listen(port);
 
 console.log('The server is running');
+
+
+/*********************************/
+/* Set up the static file server */
+
+const { Server } = require("socket.io");
+const io = new Server(app);
+
+io.on('connection', (socket) => {
+
+    /* Output a log message on the server and send it to the clients */
+    function serverLog(...messages){
+        io.emit('log', ['**** Message from the server:\n']);
+        messages.forEach((item) => {
+            io.emit('log', ['****\t'+item]);
+            console.log(item);
+        })
+    }
+
+    serverLog('a page connected to the server: '+socket.id);
+    
+    socket.on('disconnect', () => {
+        serverLog('a page disconnected from the server: '+socket.id);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
